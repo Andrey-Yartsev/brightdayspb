d3f3d6191b3bb4101be6799d9985adfb309155df
Andrey Yartsev <andrey.yartsev.g@gmail.com>
2017-07-10 13:58:08 +0300

    add "why we" block to homepage template

--- wp-content/themes/LightFirm/left.php
+++ wp-content/themes/LightFirm/left.php
@@ -16,6 +16,43 @@
 		<div class="clear"></div>
 	</div>
 
+  <div class="entry">
+    <h1>Почему мы</h1>
+    <div class="icons">
+      <div class="icon icon1">
+        <i></i>
+        <div class="title">Опыт 10 лет</div>
+        <div class="sub-title">Опыт оформления мероприятий более 10 лет</div>
+      </div>
+      <div class="icon icon2">
+        <i></i>
+        <div class="title">Мастерская</div>
+        <div class="sub-title">Собственные мастерские и современное оборудование</div>
+      </div>
+      <div class="icon icon3">
+        <i></i>
+        <div class="title">Выгодные условия</div>
+        <div class="sub-title">Выгодные условия сотрудничества для event агентств</div>
+      </div>
+      <div class="icon icon4">
+        <i></i>
+        <div class="title">Возможности</div>
+        <div class="sub-title">Изготовим любые декорации на заказ</div>
+      </div>
+      <div class="icon icon5">
+        <i></i>
+        <div class="title">Опыт 10 лет</div>
+        <div class="sub-title">Опыт оформления мероприятий более 10 лет</div>
+      </div>
+      <div class="icon icon6">
+        <i></i>
+        <div class="title">Опыт 10 лет</div>
+        <div class="sub-title">Опыт оформления мероприятий более 10 лет</div>
+      </div>
+    </div>
+    <div class="clear"></div>
+  </div>
+
 	<div style="margin-bottom: 30px;">
 		<div class="how-h1">Наши услуги</div>
 		<div class="clear"></div>
